e34fea12b44f2a9d134f665aeb9a40dfcf92385a
Vin Armani <vin@vinarmani.com>
2021-05-21 05:23:42 +0000

    enable basic postage protocol sends

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ sudo npm link
 You can now use the project from the command line with the `slpswap` command
 
 ### Command Line Usage
+
+#### SLP Swap
 ```
 slpswap --wif=<PRIVATE_KEY_FOR_ADDRESS_HOLDING_SLP_TOKENS> --amt=<AMOUNT_TO_SEND> --send=<TOKEN_ID_OF_TOKENS_BEING_SENT> --receive=<TOKEN_ID_OF_TOKENS_TO BE RECEIVED>
 ```
@@ -23,5 +25,16 @@ slpswap --wif=<PRIVATE_KEY_FOR_ADDRESS_HOLDING_SLP_TOKENS> --amt=<AMOUNT_TO_SEND
 Sample command to send 50.5 SPICE in exchange for HONK
 ```
 slpswap --wif=L4rnvJMVVPQP8J8xo7kT3YARYt9nwLrWQybkNXzCQnKp6Wj3WrrX --amt=50.5 --send=4de69e374a8ed21cbddd47f2338cc0f479dc58daa2bbe11cd604ca488eca0ddf --receive=7f8889682d57369ed0e32336f8b7e0ffec625a35cca183f4e81fde4e71a538a1
+```
+
+#### Regular transaction using Postage Protocol
+```
+slpswap --wif=<PRIVATE_KEY_FOR_ADDRESS_HOLDING_SLP_TOKENS> --amt=<AMOUNT_TO_SEND> --send=<TOKEN_ID_OF_TOKENS_BEING_SENT> --to=<SIMPLELEDGER_ADDRESS_TO_SEND_TO> --postage=true
+```
+
+
+Sample command to send 50.5 SPICE using Postage PRotocol
+```
+slpswap --wif=L4rnvJMVVPQP8J8xo7kT3YARYt9nwLrWQybkNXzCQnKp6Wj3WrrX --amt=50.5 --send=4de69e374a8ed21cbddd47f2338cc0f479dc58daa2bbe11cd604ca488eca0ddf --to=simpleledger:qzhfgts6y6pnyhrfxas9yy7rheffggcjuc3u67s8ug --postage=true
 ```
 
--- index.js
+++ index.js
@@ -38,37 +38,44 @@ if (!wif)
 let amtToSendStandard = args['amt']
 if (!amtToSendStandard)
   throw new Error ('Must include token amount as argument --amt=<AMOUNT_TO_SEND>')
+// Do a regular send with postage?
+const postage = args['postage'] && args['postage'] == 'true'
+const to = args['to']
+if (postage && !to)
+    throw new Error ('Must include recipient address to send as argument --to=<RECIPIENT_ADDRESS>')
+// Otherwise do a swap transaction
 const useToken = args['send']
   if (!useToken)
     throw new Error ('Must include token ID to send as argument --send=<TOKENID_TO_SEND>')
 const swapToken = args['receive']
-    if (!swapToken)
+    if (!swapToken && !postage)
       throw new Error ('Must include token ID to receive as argument --send=<TOKENID_TO_RECEIVE>')
 
+const rateEndpoint = postage ? PayPro.postageEndpoint : PayPro.swapRateEndpoint
 const priv = PrivateKey.fromString(wif);
 const addr = priv.toAddress();
 const cashAddress = addr.toString();
 
 (async function(){
-    try {
-      console.log(`Fetching rates from ${PayPro.swapRateEndpoint}...`)
-    const ratesObj = (await axios.get(PayPro.swapRateEndpoint)).data
+  try {
+    console.log(`Fetching ${postage ? 'postage' : 'swap'} rates from ${rateEndpoint}...`)
+    const ratesObj = (await axios.get(rateEndpoint)).data
     const exchangeAddr = bchaddr.toCashAddress(ratesObj.address)
-    const useTokenRateObj = ratesObj.tokens.find(t => t.tokenId == useToken)
+    const useTokenRateObj = ratesObj[postage ? 'stamps' : 'tokens'].find(t => t.tokenId == useToken)
     if (!useTokenRateObj)
-      throw new Error ('The token you are sending is not valid with this SLP swap provider')
-    const swapTokenRateObj = ratesObj.tokens.find(t => t.tokenId == swapToken)
-    if (!swapTokenRateObj)
+      throw new Error (`The token you are sending is not valid with this ${postage ? 'postage' : 'SLP swap'} provider`)
+    const swapTokenRateObj = postage ? undefined : ratesObj.tokens.find(t => t.tokenId == swapToken)
+    if (!swapTokenRateObj && !postage)
       throw new Error ('The token you want to receive is not offered by this SLP swap provider')
     // Convert amount to base units
     const amtToSend = amtToSendStandard * (10 ** useTokenRateObj.decimals)
     // Check that amount desired in token to be received is available
-    const paidTokenRate = Number(useTokenRateObj.buy)
-    const swapTokenRate = Number(swapTokenRateObj.sell)
+    const paidTokenRate = postage ? undefined : Number(useTokenRateObj.buy)
+    const swapTokenRate = postage ? undefined : Number(swapTokenRateObj.sell)
     // console.log('tokensPaidStandard', tokensPaidStandard)
-    const bchAmtPaid = amtToSendStandard * paidTokenRate
-    const amountToSwapStandard = (bchAmtPaid / swapTokenRate).toFixed(swapTokenRateObj.decimals)
-    if (amountToSwapStandard > swapTokenRateObj.available)
+    const bchAmtPaid = postage ? undefined : amtToSendStandard * paidTokenRate
+    const amountToSwapStandard = postage ? undefined : (bchAmtPaid / swapTokenRate).toFixed(swapTokenRateObj.decimals)
+    if (!postage && amountToSwapStandard > swapTokenRateObj.available)
       throw new Error (`The amount of ${useTokenRateObj.symbol} you are sending exceeds the amount of ${swapTokenRateObj.symbol} available to swap`)
 
     console.log(`Fetching ${useTokenRateObj.symbol} UTXOs at ${cashAddress}...`)
@@ -96,10 +103,14 @@ const cashAddress = addr.toString();
     // Create Payment Transaction
     if (amtToSend > useTokenTotal)
       throw new Error (`Insufficient tokens to complete send. Only ${useTokenTotal * (10 ** (-1 * useTokenRateObj.decimals))} ${useTokenRateObj.symbol} available`)
-    console.log(`Swapping ${amtToSendStandard} ${useTokenRateObj.symbol} for ${amountToSwapStandard} (minus any postage cost) ${swapTokenRateObj.symbol}...`)
-    console.log('Constructing SLP Swap transaction...')
+    if (!postage) {
+      console.log(`Swapping ${amtToSendStandard} ${useTokenRateObj.symbol} for ${amountToSwapStandard} (minus any postage cost) ${swapTokenRateObj.symbol}...`)
+      console.log('Constructing SLP Swap transaction...')
+    } else {
+      console.log('Constructing postage transaction...')
+    }
     const bnAmount = new BN(amtToSend) // Amount to send
-    const tokenChange = new BN(useTokenTotal - amtToSend)
+    let tokenChange = new BN(useTokenTotal - amtToSend)
     const sendOpReturnArray = [
         'OP_RETURN',
         '534c5000',
@@ -111,6 +122,11 @@ const cashAddress = addr.toString();
     // Add change output to OP_RETURN
     if (tokenChange.gt(0))
       sendOpReturnArray.push(BNToInt64BE(tokenChange).toString('hex'))
+    if (postage) {
+      // Add placeholder
+      const placeHolder = new BN(1)
+      sendOpReturnArray.push(BNToInt64BE(placeHolder).toString('hex'))
+    }
     sendOpReturnASM = sendOpReturnArray.join(' ');
     const opReturnScript = Script.fromASM(sendOpReturnASM);
 
@@ -122,27 +138,56 @@ const cashAddress = addr.toString();
           script: opReturnScript,
           satoshis: 0
       }))
-      .to(exchangeAddr, 546)
+      .to(postage ? bchaddr.toCashAddress(to) : exchangeAddr, 546)
     // Add change output
     if (tokenChange.gt(0))
       tx.to(addr, 546)
     // Add chain dust output and sign
     tx.to(exchangeAddr, 546)
-      .sign([priv], sighash);
+    if (!postage)
+     tx.sign([priv], sighash);
 
-    const hex = tx.toString()
-    // console.log(tx.toObject())
+    let hex = tx.toString()
+
+    if (postage) {
+      // get tx size
+      const byteCount = hex.length / 2
+      // Calculate number of Stamps Needed
+      const outputSum = tx.outputs.reduce(function(accumulator, output){
+        return accumulator + output.satoshis
+      }, 0)
+      // console.log('utxos', utxos)
+      const inputSum = utxos.reduce((total, input) => {
+          return total + input.satoshis
+      }, 0)
+      const stampsNeeded = Math.ceil((outputSum + byteCount - inputSum) / ratesObj.weight)
+      console.log(`Paying for ${stampsNeeded} stamps`)
+      const stampsBnAmount = new BN(stampsNeeded * (useTokenRateObj.rate * (10 ** useTokenRateObj.decimals)))
+      tokenChange = tokenChange.minus(stampsBnAmount)
+      if (tokenChange.lt(0))
+        throw new Error ('Not enough funds available to cover postage')
+      const newSendOpReturnArray = sendOpReturnArray.slice(0, 6)
+      newSendOpReturnArray.push(BNToInt64BE(tokenChange).toString('hex'))
+      newSendOpReturnArray.push(BNToInt64BE(stampsBnAmount).toString('hex'))
+      const newSendOpReturnASM = newSendOpReturnArray.join(' ');
+      const newOpReturnScript = Script.fromASM(newSendOpReturnASM);
+      tx.outputs[0] = new Transaction.Output({
+        script: newOpReturnScript,
+        satoshis: 0
+      })
+      // sign
+      tx.sign([priv], sighash);
+      hex = tx.toString()
+    }
     console.log('Sending to SLP Swap API (Postage Endpoint):', hex)
     
     // Broadcast tx
     const txIds = await PayPro.broadcastPostOfficeTx(
       hex,
       cashAddress,
-      {
-        slpSwap: swapToken
-      }
+      postage ? {} : { slpSwap: swapToken }
     );
-    console.log('Success! Swap transaction IDs', txIds)
+    console.log(`Success! ${postage? 'Postage' : 'Swap'} transaction IDs`, txIds)
    
 
   } catch (e) {
